fd827eba2b3caf9cde59d0b7dff4a0c975ff7a6c
sizigia <cumbresdigitales@gmail.com>
2019-11-11 11:57:25 -0300

    Updates

---
+++ precache-manifest.d6b3430e1ea9c96c356fb0f876d960ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb7f9b40e26d888b3c9e374294f5b6ed",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "313896aac0aafa379b21",
+    "url": "/react-tutorial/static/css/main.bf286928.chunk.css"
+  },
+  {
+    "revision": "bd466c1677455ec11308",
+    "url": "/react-tutorial/static/js/2.f106a7b4.chunk.js"
+  },
+  {
+    "revision": "313896aac0aafa379b21",
+    "url": "/react-tutorial/static/js/main.b78b2b01.chunk.js"
+  },
+  {
+    "revision": "ce837e94642904d34122",
+    "url": "/react-tutorial/static/js/runtime-main.30a083f0.js"
+  }
+]);
